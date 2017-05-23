f77854e87b6514883f5ea0421e59dca5c7436c16
Peder Waern <pederwaern@hotmail.com>
2017-05-23 14:44:16 +0200

    first commit

---
+++ src/main/java/se/nackademin/mvcgruppen/CookbookApplication.java
@@ -0,0 +1,12 @@
+package se.nackademin.mvcgruppen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookbookApplication.class, args);
+	}
+}
