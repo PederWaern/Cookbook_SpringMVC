af36155d683b807214aa5cb37822b95a1dd9916a
daniellindkarlberg <danielskua@hotmail.com>
2017-05-24 10:09:10 +0200

    added recipe upload etc

--- src/main/java/se/nackademin/mvcgruppen/controller/HomeController.java
+++ src/main/java/se/nackademin/mvcgruppen/controller/HomeController.java
@@ -1,13 +1,38 @@
 package se.nackademin.mvcgruppen.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import se.nackademin.mvcgruppen.domain.Recipe;
+import se.nackademin.mvcgruppen.service.RecipeService;
+import se.nackademin.mvcgruppen.service.UploadService;
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/home")
-    public String home(){
+    @Autowired
+    private RecipeService recipeService;
+    @Autowired
+    private UploadService uploadService;
+
+    @GetMapping("/recipe")
+    public String recipeForm(Model model) {
+        model.addAttribute("recipe", new Recipe());
+        return "home";
+    }
+
+
+    @PostMapping("/recipe")
+    public String recipeSubmit(@RequestParam(value = "file") MultipartFile image,
+                               @ModelAttribute Recipe recipe) {
+
+        recipe.setImgUrl(uploadService.cloudUpload(image));
+        recipeService.saveRecipe(recipe);
         return "home";
     }
 
