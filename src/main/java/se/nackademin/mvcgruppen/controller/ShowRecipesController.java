2627c5dde32739aabf5d61a2c7da20d83ace46d5
Peder Waern <pederwaern@hotmail.com>
2017-05-29 14:50:19 +0200

    fixed change portions

--- src/main/java/se/nackademin/mvcgruppen/controller/ShowRecipesController.java
+++ src/main/java/se/nackademin/mvcgruppen/controller/ShowRecipesController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import se.nackademin.mvcgruppen.domain.Recipe;
 import se.nackademin.mvcgruppen.service.RecipeService;
 
@@ -18,7 +20,7 @@ public class ShowRecipesController {
     private RecipeService recipeService;
 
     private List<Integer> portionValues = Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12);
-    private int currentPortions;
+    private Recipe oldRecipe;
 
 
     @GetMapping("/recipes")
@@ -28,15 +30,36 @@ public class ShowRecipesController {
     }
 
     @GetMapping("/recipes/{id}")
-    public String showRecipe(@PathVariable Integer id, Model model) {
+    public String showRecipe(@PathVariable int id, Model model) {
 
         Recipe currentRecipe = recipeService.getRecipeById(id);
-
-
+        oldRecipe = currentRecipe;
         model.addAttribute("recipe", currentRecipe);
         model.addAttribute("portionValues", portionValues);
 
         System.out.println(portionValues);
         return "showrecipe";
+    }
+    @PostMapping("/recipes/changedPortion")
+    public String portionChanged(@ModelAttribute Recipe recipe, Model model) {
+
+        System.out.println(recipe.getPortion());
+        model.addAttribute("recipe",updatedRecipe(recipe.getPortion()));
+        model.addAttribute("portionValues", portionValues);
+
+        return "showrecipe";
+    }
+
+    private Recipe updatedRecipe(int newPortionValue) {
+        Recipe updatedRecipe = oldRecipe;
+
+        updatedRecipe.getIngredients().forEach
+                (ingredient -> ingredient.setQuantity(ingredient.getQuantity() / updatedRecipe.getPortion()));
+        updatedRecipe.getIngredients().forEach(
+                ingredient -> ingredient.setQuantity(ingredient.getQuantity() * newPortionValue));
+
+        updatedRecipe.setPortion(newPortionValue);
+
+        return updatedRecipe;
     }
 }
